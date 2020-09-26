742048554d09db4565a9c2138f78fcbc9f01542e
ismology <toni_vasilenko@mail.ru>
2020-09-26 13:37:05 +0600

    lemp save_work reset_to_snapshot

--- lemp_install.sh
+++ lemp_install.sh
@@ -1,2 +1,23 @@
 #!/usr/bin/env
 
+## Variables ##
+$mysql_passwd = "ismology12"
+
+## Step 1 – Installing the Nginx Web Server ##
+apt update
+apt install nginx
+# Configure UFW
+ufw allow 'Nginx HTTP'
+ufw allow 'Nginx HTTPS'
+ufw enable
+
+## Step 2 – Installing MySQL to Manage Site Data ##
+apt install mysql-server mysql-client -y
+mysql > << EOF
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$mysql_passwd';
+FLUSH PRIVILEGES;
+EOF
+
+mysql -D mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'ismology12';"
+FLUSH PRIVILEGES;
+
